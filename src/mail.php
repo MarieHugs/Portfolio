c4ee4132590ff593e118ab7b421c75956e93e663
Marie-Eve Lamontagne <marie-evelamontagne1@hotmail.com>
2021-10-18 16:53:22 -0400

    Continuité du php pour l'envoie du mail non fonctionnel en ce moment

--- src/mail.php
+++ src/mail.php
@@ -1,10 +1,14 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "1767954@cegepmontpetit.ca";
-$subject = "Formulaire de Contact - Portfolio";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Merci!";
+<?php 
+$to      = 'marie-evelamontagne1@hotmail.com';
+    $message = $this->input->post('message');
+    $from_email="=?UTF-8?B?".base64_encode( $this->input->post('email'))."?=";
+    $from_user = "=?UTF-8?B?".base64_encode( $this->input->post('name'))."?=";
+ 
+    $headers = "From: $from_user <$from_email>\r\n".   "MIME-Version: 1.0" . "\r\n" .
+        "Content-type: text/html; charset=UTF-8" . "\r\n";
+ 
+    mail($to, $subject, $message, $headers);
+    redirect('contact/?m=added');
 ?>
+
+//https://openclassrooms.com/forum/sujet/envoi-d-un-mail-depuis-un-formulaire-html
